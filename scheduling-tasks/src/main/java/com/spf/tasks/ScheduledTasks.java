32f257157e986b993610615c758172d9fc98f532
shipfeiw <527615754@qq.com>
2018-05-25 15:10:12 +0800

    init

---
+++ scheduling-tasks/src/main/java/com/spf/tasks/ScheduledTasks.java
@@ -0,0 +1,27 @@
+package com.spf.tasks;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/*
+ * created by shipengfei
+ * on 2018/5/25, 13:41
+ * description 
+ */
+@Component
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime(){
+        log.info("This time is now {}", dateFormat.format(new Date()));
+    }
+}
